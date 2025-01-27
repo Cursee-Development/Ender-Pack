4d7b3cc9ea58c93d617e70605e99fc0d4cd8e859
jason13official <jason13official@gmail.com>
2025-01-26 19:52:24 -0500

    textures, creative mode tab

--- forge/src/main/java/com/cursee/ender_pack/core/registry/RegistryForge.java
+++ forge/src/main/java/com/cursee/ender_pack/core/registry/RegistryForge.java
@@ -1,10 +1,16 @@
 package com.cursee.ender_pack.core.registry;
 
+import com.cursee.ender_pack.Constants;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegisterEvent;
@@ -21,8 +27,21 @@ public class RegistryForge {
             ModBlocks.register(consumer);
         });
         bindForItems(ModItems::register);
-
         bind(Registries.BLOCK_ENTITY_TYPE, ModBlockEntities::register);
+
+        bind(Registries.CREATIVE_MODE_TAB, consumer -> {
+            consumer.accept(CreativeModeTab.builder()
+                            .icon(() -> new ItemStack(ModItems.ENDER_PACK))
+                            .title(Component.translatable("itemGroup.enderPack"))
+                            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+                            .build(),
+                    Constants.ENDER_PACK_TAB_KEY.location());
+        });
+        modEventBus.addListener((BuildCreativeModeTabContentsEvent entries) -> {
+            if (entries.getTabKey() == Constants.ENDER_PACK_TAB_KEY) {
+                CREATIVE_MODE_TAB_ITEMS.forEach(entries::accept);
+            }
+        });
     }
 
     private static <T> void bind(ResourceKey<Registry<T>> registry, Consumer<BiConsumer<T, ResourceLocation>> source) {
